1cc58bdfe3b250c0de5dbb4557ecf1fd8b3c4f60
rojhat-hva <73133346+rojhat-hva@users.noreply.github.com>
2020-11-19 14:01:58 +0100

    Add files via upload

---
+++ angularHtmlInputBear.py
@@ -0,0 +1,28 @@
+import logging
+
+from coalib.bears.LocalBear import LocalBear
+
+class angularHtmlInputBear(LocalBear):
+   def run(self,
+            filename,
+            file):
+            for line in file:
+                if 'input'   in line:
+                    logging.debug("INPUT!")
+                    yield self.new_result("Found input:" + line + ".", file=filename)
+               
+                else:
+                    logging.debug("Checking line")
+ 		
+class angularHtmlCheckBear(LocalBear):
+    def run(self,
+            filename,
+            file):
+            for line in file:
+                if 'href='   in line:
+                    logging.debug("Found URL!")
+                    yield self.new_result("Found URL`s in CODE:" + line + ".", file=filename)
+              	
+                else:
+                    logging.debug("Checking line")
+ 		
